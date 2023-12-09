beddf14313e8dd074d17853859394c0713420bbb
mladinalina2002 <47451625+mladinalina2002@users.noreply.github.com>
2023-12-09 16:50:57 +0200

    Update Table.java

--- src/main/java/ro/uvt/models/Table.java
+++ src/main/java/ro/uvt/models/Table.java
@@ -1,6 +1,7 @@
 package ro.uvt.models;
 
 import lombok.Data;
+import ro.uvt.services.Visitor;
 
 @Data
 public class Table implements Element{
@@ -27,5 +28,10 @@ public class Table implements Element{
     @Override
     public Element get(int id) {
         return null;
+    }
+
+    @Override
+    public void accept(Visitor v) {
+        v.visitTable(this);
     }
 }
