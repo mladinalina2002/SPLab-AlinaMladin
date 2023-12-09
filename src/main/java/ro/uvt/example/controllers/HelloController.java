7c0a21010abcb5f677ab66153dc5389abf09f220
mladinalina2002 <47451625+mladinalina2002@users.noreply.github.com>
2023-12-09 16:46:45 +0200

    Update HelloController.java

--- src/main/java/ro/uvt/example/controllers/HelloController.java
+++ src/main/java/ro/uvt/example/controllers/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ro.uvt.example.ClientComponent;
 
-
 @RestController
 @RequestMapping("/")
 public class HelloController {
